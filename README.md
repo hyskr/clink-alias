5e3d7ff9c638f5025f1dbd65eb97ab703c94b8b5
Cosmic <65348517+hyskr@users.noreply.github.com>
2024-11-28 05:44:12 +0000

    add clink_start.cmd

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # clink-alias
+
+install lsd by `cargo install lsd` 
+cp `clink_start.cmd` to `%AppData%/../local/clink`
---
+++ clink_start.cmd
@@ -0,0 +1,12 @@
+@echo off
+doskey rm=del
+doskey cp=copy
+doskey mv=move
+doskey clear=cls
+doskey cat=type $1
+doskey ls = lsd
+doskey l=lsd -l
+doskey ll=lsd -l
+doskey la=lsd -a
+doskey lla=lsd -la
+doskey lt=lsd --tree
